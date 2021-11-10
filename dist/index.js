d9f8a23b0ce598d2de61fefd28615a91638fc2f7
Diego Silva <drsilva@gmail.com>
2021-11-10 18:39:30 -0300

    ajuste

--- dist/index.js
+++ dist/index.js
@@ -15132,7 +15132,7 @@ function createAdaptiveCard(author, authorName, message, prTitle, prUrl, prNum, 
                 items: [
                     {
                         type: 'TextBlock',
-                        text: prTitle,
+                        text: "prTitle",
                         weight: 'bolder',
                         size: 'medium'
                     },
@@ -15219,7 +15219,145 @@ function createAdaptiveCard(author, authorName, message, prTitle, prUrl, prNum, 
             }
         ]
     };
-    return adaptiveCard;
+    const teste = {
+        "$schema": "https://adaptivecards.io/schemas/adaptive-card.json",
+        "type": "AdaptiveCard",
+        "version": "1.0",
+        "body": [
+            {
+                "type": "Container",
+                "items": [
+                    {
+                        "type": "TextBlock",
+                        "text": "Publish Adaptive Card schema",
+                        "weight": "bolder",
+                        "size": "medium"
+                    },
+                    {
+                        "type": "ColumnSet",
+                        "columns": [
+                            {
+                                "type": "Column",
+                                "width": "auto",
+                                "items": [
+                                    {
+                                        "type": "Image",
+                                        "url": "https://connectorsdemo.azurewebsites.net/images/MSC12_Oscar_002.jpg",
+                                        "size": "small",
+                                        "style": "person",
+                                        "altText": "Migeul Garcia's Profile Picture"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "Column",
+                                "width": "stretch",
+                                "items": [
+                                    {
+                                        "type": "TextBlock",
+                                        "text": "Miguel Garcia",
+                                        "weight": "bolder",
+                                        "wrap": true
+                                    },
+                                    {
+                                        "type": "TextBlock",
+                                        "spacing": "none",
+                                        "text": "Created {{DATE(2017-02-14T06:08:39Z, SHORT)}}",
+                                        "isSubtle": true,
+                                        "wrap": true
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "type": "Container",
+                "items": [
+                    {
+                        "type": "TextBlock",
+                        "text": "Now that we have defined the main rules and features of the format, we need to produce a schema and publish it to GitHub. The schema will be the starting point of our reference documentation.",
+                        "wrap": true
+                    },
+                    {
+                        "type": "FactSet",
+                        "facts": [
+                            {
+                                "title": "Board:",
+                                "value": "Adaptive Card"
+                            },
+                            {
+                                "title": "List:",
+                                "value": "Backlog"
+                            },
+                            {
+                                "title": "Assigned to:",
+                                "value": "Matt Hidinger"
+                            },
+                            {
+                                "title": "Due date:",
+                                "value": "Not set"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        "actions": [
+            {
+                "type": "Action.ShowCard",
+                "title": "Set due date",
+                "card": {
+                    "type": "AdaptiveCard",
+                    "body": [
+                        {
+                            "type": "Input.Date",
+                            "id": "dueDate",
+                            "isRequired": true,
+                            "placeholder": "Enter due date"
+                        }
+                    ],
+                    "actions": [
+                        {
+                            "type": "Action.Http",
+                            "method": "POST",
+                            "body": "{{dueDate.value}}",
+                            "title": "OK",
+                            "url": "https://messagecardplaygroundfn.azurewebsites.net/"
+                        }
+                    ]
+                }
+            },
+            {
+                "type": "Action.ShowCard",
+                "title": "Comment",
+                "card": {
+                    "type": "AdaptiveCard",
+                    "body": [
+                        {
+                            "type": "Input.Text",
+                            "id": "comment",
+                            "isRequired": true,
+                            "isMultiline": true,
+                            "placeholder": "Enter your comment"
+                        }
+                    ],
+                    "actions": [
+                        {
+                            "type": "Action.Http",
+                            "method": "POST",
+                            "body": "{{comment.value}}",
+                            "title": "OK",
+                            "url": "https://messagecardplaygroundfn.azurewebsites.net/"
+                        }
+                    ]
+                }
+            }
+        ]
+    };
+    // return adaptiveCard
+    return teste;
 }
 exports.createAdaptiveCard = createAdaptiveCard;
 
