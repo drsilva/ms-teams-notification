9ca0ad6b1ab676de9bad8fcb4ad5af81f186876a
Diego Silva <drsilva@gmail.com>
2021-11-11 08:17:43 -0300

    Compilado

--- dist/index.js
+++ dist/index.js
@@ -2454,40 +2454,31 @@ function createMessageCard(notificationSummary, notificationColor, author, autho
         summary: notificationSummary,
         themeColor: notificationColor,
         title: notificationSummary,
-        items: [
-            {
-                type: 'FactSet',
-                facts: [
-                    {
-                        title: 'PR #:',
-                        value: 'prNum'
-                    },
-                    {
-                        title: 'Repositório:',
-                        value: 'repoName'
-                    },
-                    {
-                        title: 'Branch Origem:',
-                        value: 'branchTarget'
-                    },
-                    {
-                        title: 'Branch Destino:',
-                        value: 'branchDest'
-                    },
-                    {
-                        title: 'Data:',
-                        value: timestamp
-                    }
-                ]
-            }
-        ],
         sections: [
             {
                 activityTitle: `${prTitle}`,
                 activityImage: avatar_url,
-                activityText: `${message}` +
+                text: `${message}` +
                     `<br>` +
-                    `Autor: <b>${authorName}</b> [(@${author.login})](${author.html_url}) em ${timestamp}`
+                    `Autor: <b>${authorName}</b> [(@${author.login})](${author.html_url}) em ${timestamp}`,
+                "facts": [
+                    {
+                        "name": "Board:",
+                        "value": "Name of board"
+                    },
+                    {
+                        "name": "List:",
+                        "value": "Name of list"
+                    },
+                    {
+                        "name": "Assigned to:",
+                        "value": "(none)"
+                    },
+                    {
+                        "name": "Due date:",
+                        "value": "(none)"
+                    }
+                ],
             }
         ],
         potentialAction: [
@@ -2499,7 +2490,118 @@ function createMessageCard(notificationSummary, notificationColor, author, autho
             }
         ]
     };
-    return messageCard;
+    const teste = {
+        "@type": "MessageCard",
+        "@context": "https://schema.org/extensions",
+        "summary": "Card \"Test card\"",
+        "themeColor": "0078D7",
+        "title": "Card created: \"Name of card\"",
+        "sections": [
+            {
+                "activityTitle": "Miguel Garcia",
+                "activitySubtitle": "9/13/2016, 3:34pm",
+                "activityImage": "https://connectorsdemo.azurewebsites.net/images/MSC12_Oscar_002.jpg",
+                "facts": [
+                    {
+                        "name": "Board:",
+                        "value": "Name of board"
+                    },
+                    {
+                        "name": "List:",
+                        "value": "Name of list"
+                    },
+                    {
+                        "name": "Assigned to:",
+                        "value": "(none)"
+                    },
+                    {
+                        "name": "Due date:",
+                        "value": "(none)"
+                    }
+                ],
+                "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
+        ],
+        "potentialAction": [
+            {
+                "@type": "ActionCard",
+                "name": "Set due date",
+                "inputs": [
+                    {
+                        "@type": "DateInput",
+                        "id": "dueDate",
+                        "title": "Select a date"
+                    }
+                ],
+                "actions": [
+                    {
+                        "@type": "HttpPOST",
+                        "name": "OK",
+                        "target": "http://..."
+                    }
+                ]
+            },
+            {
+                "@type": "ActionCard",
+                "name": "Move",
+                "inputs": [
+                    {
+                        "@type": "MultichoiceInput",
+                        "id": "move",
+                        "title": "Pick a list",
+                        "choices": [
+                            {
+                                "display": "List 1",
+                                "value": "l1"
+                            },
+                            {
+                                "display": "List 2",
+                                "value": "l2"
+                            }
+                        ]
+                    }
+                ],
+                "actions": [
+                    {
+                        "@type": "HttpPOST",
+                        "name": "OK",
+                        "target": "http://..."
+                    }
+                ]
+            },
+            {
+                "@type": "ActionCard",
+                "name": "Add a comment",
+                "inputs": [
+                    {
+                        "@type": "TextInput",
+                        "id": "comment",
+                        "isMultiline": true,
+                        "title": "Enter your comment"
+                    }
+                ],
+                "actions": [
+                    {
+                        "@type": "HttpPOST",
+                        "name": "OK",
+                        "target": "http://..."
+                    }
+                ]
+            },
+            {
+                "@type": "OpenUri",
+                "name": "View in Trello",
+                "targets": [
+                    {
+                        "os": "default",
+                        "uri": "http://..."
+                    }
+                ]
+            }
+        ]
+    };
+    // return messageCard
+    return teste;
 }
 exports.createMessageCard = createMessageCard;
 
